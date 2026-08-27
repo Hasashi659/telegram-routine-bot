9440f7f975a61792113ea5192b0d7aa42db07bdf
Juanito Petacas <dj056858@gmail.com>
2026-08-27 15:22:13 -0500

    Hardcode token to fix Unauthorized error

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ logger = logging.getLogger(__name__)
 # CONFIGURACIÓN
 # ============================================
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '8866031857:AAFebwtF2BtmzQNYJQnT6t2r1ntqH38fhDo')
-OWNER_CHAT_ID = os.environ.get('CHAT_ID', '6613206978')
+BOT_TOKEN = os.environ.get('BOT_TOKEN') or '8866031857:AAFebwtF2BtmzQNYJQnT6t2r1ntqH38fhDo'
+OWNER_CHAT_ID = os.environ.get('CHAT_ID') or '6613206978'
 
 # ============================================
 # HORARIO DE LA RUTINA
